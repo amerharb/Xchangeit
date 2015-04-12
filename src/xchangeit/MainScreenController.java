cd8a09f865b11c69ba9f4ae3586c91ee557897f6
amerharb <amer.harb@gmail.com>
2015-04-12 22:40:58 +0200

    add Currency Stage

--- src/xchangeit/MainScreenController.java
+++ src/xchangeit/MainScreenController.java
@@ -6,28 +6,46 @@
 
 package xchangeit;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Amer
  */
-public class MainScreenController implements Initializable
+public class MainScreenController implements Initializable 
 {
-    
-    @FXML
-    private Label label;
+    Stage CurrencyStage = new Stage();
     
     @FXML
     private void handleButtonAction(ActionEvent event)
     {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        try {
+            System.out.println("You clicked Currency");
+            
+            Parent root = FXMLLoader.load(getClass().getResource("Currency.fxml"));
+            
+            Scene scene = new Scene(root);
+            
+     
+            CurrencyStage.setScene(scene);
+            CurrencyStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(MainScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
     
     @Override
