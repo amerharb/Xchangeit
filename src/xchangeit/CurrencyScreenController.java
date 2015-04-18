a3d0e442af34f8e1e4ee86e5fb9b6f088092a5f3
amerharb <amer.harb@gmail.com>
2015-04-18 07:07:16 +0200

    pass the connection to currency control and test take data via Currency class

--- src/xchangeit/CurrencyScreenController.java
+++ src/xchangeit/CurrencyScreenController.java
@@ -7,24 +7,48 @@
 package xchangeit;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
  *
  * @author Amer
  */
-public class CurrencyScreenController implements Initializable
+public class CurrencyScreenController extends XchController
 {
 
     /**
      * Initializes the controller class.
      */
+    @FXML
+    TableView currencTable;
+    XchangeitDatabase DataBase;
+    
+    @FXML TableColumn pkCol;
+    @FXML TableColumn nameCol;
+    @FXML TableColumn isoCol;
+    @FXML TableColumn symbolCol;
+    @FXML TableColumn noteCol;
+    
+    ArrayList<Currency> allCureency;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
-    }    
+        DataBase = MainScreen.getDatabase();
+        allCureency = DataBase.getAllCurrency();
+        fillTable();
+    }
+    
+    private void fillTable(){
+        //TODO
+        for(Currency c:allCureency){
+            System.out.println(c.getCurrName());
+        }
+    }
     
 }
