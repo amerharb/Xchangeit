68eb26fc49792392f7b746950afdb4d3f81ed25a
amerharb <amerharb@gmail.com>
2015-05-03 02:21:08 +0200

    create 2 classes for buy and sell

---
+++ src/xchangeit/buy/Buy.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xchangeit.buy;
+
+import java.util.Date;
+import xchangeit.Transaction;
+import xchangeit.XchTransactionTypeeEnum;
+import static xchangeit.XchTransactionTypeeEnum.CashOut;
+import xchangeit.cashOut.CashOut;
+import xchangeit.currIn.CurrIn;
+import xchangeit.currency.Currency;
+
+/**
+ *
+ * @author Amer
+ */
+public class Buy extends CurrIn
+{
+    private double cashAmount;
+    private double price;
+    
+    public double getCashAmount()
+    {
+        return cashAmount;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    @Override
+    public double getCash()
+    {
+        return -cashAmount;
+    }
+
+    @Override
+    public XchTransactionTypeeEnum getTransType()
+    {
+        return XchTransactionTypeeEnum.Buy;
+    }
+    
+    public Buy(int pk, Date transDate, String note, Currency currency, 
+            double currAmount, double rate, double cashAmount, double price)
+    {
+        super(pk, transDate, note, currency, currAmount, rate);
+        this.cashAmount = Math.abs(cashAmount);
+        this.price = Math.abs(price);
+    }
+    
+    @Override
+    public double getSellBuyPrice()
+    {
+        return price;
+    }
+            
+}
---
+++ src/xchangeit/sell/Sell.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xchangeit.sell;
+
+import java.util.Date;
+import xchangeit.XchTransactionTypeeEnum;
+import xchangeit.buy.Buy;
+import xchangeit.currency.Currency;
+
+/**
+ *
+ * @author Amer
+ */
+public class Sell extends Buy
+{
+
+    public Sell(int pk, Date transDate, String note, Currency currency, double currAmount, double rate, double cashAmount, double price)
+    {
+        super(pk, transDate, note, currency, currAmount, rate, cashAmount, price);
+    }
+    
+    
+    @Override
+    public double getCash()
+    {
+        return super.getCashAmount();
+    }
+
+    @Override
+    public XchTransactionTypeeEnum getTransType()
+    {
+        return XchTransactionTypeeEnum.Sell;
+    }
+
+    @Override
+    public double getCurrAmt()
+    {
+        return -super.getCurrAmount();
+    }
+}
