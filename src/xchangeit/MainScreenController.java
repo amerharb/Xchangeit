1834f10d091f74b4edbcb3770d69556dcfe79a45
AbuIlyas <AbuIlyas@AbuIlyas-PC.lan>
2015-05-10 20:25:32 +0200

    System.out.println changes

--- src/xchangeit/MainScreenController.java
+++ src/xchangeit/MainScreenController.java
@@ -18,7 +18,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
