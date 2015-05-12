1dd7a0732210f6c8664aa84c0c227da74d6c7956
amerharb <amerharb@gmail.com>
2015-05-12 17:31:01 +0200

    fix fxid for the Buy screen

--- src/xchangeit/buy/BuyScreenController.java
+++ src/xchangeit/buy/BuyScreenController.java
@@ -8,7 +8,13 @@ package xchangeit.buy;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import xchangeit.XchController;
+import xchangeit.currency.CurrencyProperty;
 
 /**
  * FXML Controller class
@@ -21,6 +27,15 @@ public class BuyScreenController extends XchController
     /**
      * Initializes the controller class.
      */
+    
+    @FXML private DatePicker transDateDatePicker;
+    @FXML private TextField cashText;
+    @FXML private ChoiceBox<CurrencyProperty> currChoiceBox;
+    @FXML private TextField currAmtText;
+    @FXML private TextField rateText;
+    @FXML private TextField SellBuyPriceText;
+    @FXML private TextArea noteText;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
