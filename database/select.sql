98d30a46af23e69ee6490c516fa6f6e4c4891e38
amerharb <amerharb@gmail.com>
2015-05-22 01:22:27 +0200

    new select for latest rates

--- database/select.sql
+++ database/select.sql
@@ -11,6 +11,11 @@ select c.ISO_symbol currency ,r.rate ,r.sell_price ,r.buy_price
 ;
 
 
+#Last Currency rates and Prices just Currency id
+select c.pk curr ,r.rate rate ,r.sell_price sell_price ,r.buy_price buy_price from (select max(rate_date) rate_date ,curr from rates group by curr order by rate_date desc ,pk desc) as lr left join rates r on r.rate_date = lr.rate_date and r.curr = lr.curr left join curr c on r.curr = c.pk 
+;
+
+
 #current cash balance
 select sum(cash) as Balance from trans;
 
@@ -18,5 +23,9 @@ select sum(cash) as Balance from trans;
 select c.ISO_symbol Currency, c.symbol Symbol ,sum(curr_amt) as balance ,avg(t.rate) as "AVG rate" ,max(t.sell_buy_price) as "Max Price" ,min(t.sell_buy_price) as "Min Price"
 	from curr c left join trans t on t.curr = c.pk
 	group by c.pk
+;
+
+#All trans
+select * from trans
 ;
 
